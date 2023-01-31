436f8a3ea980990a704b1f9d37404a99de911ff6
Shubham Jain <someshubham@gmail.com>
2023-01-31 17:22:42 +0530

    feat(swap): add swap button

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -11,27 +11,32 @@ function App() {
       <div className="flex justify-end mr-4 mt-4">
         <ConnectWalletButton />
       </div>
-      <div className="w-500 md:w-70% flex justify-start flex-col m-8 bg-slight-black text-grey-font rounded-lg p-4">
-        <div className="text-sm mb-6">You send</div>
-        <div className="flex justify-start">
-          <TokenSelectDropDown
-            tokenName="FIL"
-            tokenBalance="11112.23"
-            tokenLogo={fileCoinLogo}
-          />
-          <div className="ml-2" />
-          <TokenQuantityInput />
+      <div className="flex flex-row justify-center">
+        <div className="flex justify-start flex-col m-8 bg-slight-black text-grey-font rounded-lg p-4">
+          <div className="text-sm mb-6">You send</div>
+          <div className="flex justify-start">
+            <TokenSelectDropDown
+              tokenName="FIL"
+              tokenBalance="11112.23"
+              tokenLogo={fileCoinLogo}
+            />
+            <div className="ml-2" />
+            <TokenQuantityInput />
+          </div>
+          <div className="mb-8" />
+          <hr className="border-divider-dark border" />
+          <div className="mb-4" />
+          <div className="text-sm mb-6">You receive</div>
+          <div className="flex justify-start">
+            <TokenSelectDropDown />
+            <div className="ml-2" />
+            <TokenQtyValueView tokenQuantity="12.23" tokenPrice="0.00" />
+          </div>
+          <div className="mb-4" />
         </div>
-        <div className="mb-8" />
-        <hr className="border-divider-dark border" />
-        <div className="mb-4" />
-        <div className="text-sm mb-6">You receive</div>
-        <div className="flex justify-start">
-          <TokenSelectDropDown />
-          <div className="ml-2" />
-          <TokenQtyValueView tokenQuantity="12.23" tokenPrice="0.00" />
-        </div>
-        <div className="mb-4" />
+      </div>
+      <div className="flex justify-center">
+        <button class="rounded-full bg-white px-20 py-3 text-xl">Swap</button>
       </div>
     </div>
   );
