cdf81920dcf56aa136f0b9da647a5c11c742b330
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2023-02-01 19:01:08 +0530

    fix token decimals

--- contracts/tokens/FUSD.sol
+++ contracts/tokens/FUSD.sol
@@ -5,11 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FUSDToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("FileUSD", "FUSD") {
-        uint256 totalSupply = initialSupply * 10 ** 6;
+        uint256 totalSupply = initialSupply * 10 ** 18;
         _mint(msg.sender, totalSupply);
-    }
-
-      function decimals() public view virtual override returns (uint8) {
-        return 6;
     }
 }
