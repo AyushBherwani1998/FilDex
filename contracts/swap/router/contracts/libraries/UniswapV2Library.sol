da81fdeb00e848402b77e8f73b3fc27782bfbe6e
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2023-02-02 22:26:28 +0530

    update factory contract address

--- contracts/swap/router/contracts/libraries/UniswapV2Library.sol
+++ contracts/swap/router/contracts/libraries/UniswapV2Library.sol
@@ -21,7 +21,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'5acb595a1c12a8a7ec06a67ca863d9fe26597efa10683e8afeb81cbb54a47c4f' // init code hash
             ))));
     }
 
