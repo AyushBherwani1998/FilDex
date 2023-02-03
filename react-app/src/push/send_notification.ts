3a333f87c193c6352b5713f677d280d17d1f2064
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2023-02-03 10:35:45 +0530

    add send notification for push

---
+++ react-app/src/push/send_notification.ts
@@ -0,0 +1,36 @@
+import * as PushAPI from "@pushprotocol/restapi";
+import * as ethers from "ethers";
+
+
+const PK = process.env.REACT_APP_PRIVATE_KEY;
+const Pkey = `0x${PK}`;
+const signer = new ethers.Wallet(Pkey);
+const channelAddress = "";
+
+async function sendNotification(title: string, body: string, recipient: string) {
+    try {
+        const apiResponse = await PushAPI.payloads.sendNotification({
+            signer,
+            type: 3,
+            identityType: 2,
+            notification: {
+                title: title,
+                body: body
+            },
+            payload: {
+                title: title,
+                body: body,
+                img: '',
+                cta: '',
+            },
+            recipients: `eip155:5:${recipient}`,
+            channel: `eip155:5:${channelAddress}`,
+            env: 'staging'
+        });
+        console.log('API repsonse: ', apiResponse);
+    } catch (err) {
+        console.error('Error: ', err);
+    }
+}
+
+export default sendNotification;
