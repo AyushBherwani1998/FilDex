8a1cc4e09191df18509cdb7d44a0b095b21b1515
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2023-02-03 12:53:37 +0530

    remove unused import form index.js

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -15,14 +15,6 @@ root.render(
   </MetaMaskProvider>
 );
 
-window.onload = () => {
-  send({
-    from: '0x458B534Bb7857F5b9A761D71ffA40f41B6c6D51b',
-    to: '0x458B534Bb7857F5b9A761D71ffA40f41B6c6D51b',
-    value: '1000000000000000'
-  });
-}
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
