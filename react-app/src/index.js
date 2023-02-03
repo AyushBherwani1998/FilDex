6050b01d34268b0d7c4c9835f3722d4f1d7c8c3e
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2023-02-03 12:54:10 +0530

    remove unused import

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { MetaMaskProvider } from "metamask-react";
-import send from "./utils/send_transaction";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
