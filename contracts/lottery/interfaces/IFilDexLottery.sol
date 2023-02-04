665196ffcb8205e7e2b021ddfbea34cab85c45e5
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2023-02-04 17:35:02 +0530

    add lottery contract

---
+++ contracts/lottery/interfaces/IFilDexLottery.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+interface IFilDexLottery {
+    function buyTickets(uint256 _lotteryId, uint32 _ticketNumber)
+        external;
+
+
+    function claimTickets(
+        uint256 _lotteryId,
+        uint256 _ticketId,
+        uint32 _bracket
+    ) external;
+
+    /**
+     * @notice Close lottery
+     * @param _lotteryId: lottery id
+     * @dev Callable by operator
+     */
+    function closeLottery(uint256 _lotteryId) external;
+
+    /**
+     * @notice Draw the final number, calculate reward in FDEX per group, and make lottery claimable
+     * @param _lotteryId: lottery id
+     * @param _autoInjection: reinjects funds into next lottery (vs. withdrawing all)
+     * @dev Callable by operator
+     */
+    function drawFinalNumberAndMakeLotteryClaimable(
+        uint256 _lotteryId,
+        bool _autoInjection
+    ) external;
+
+    /**
+     * @notice Inject funds
+     * @param _lotteryId: lottery id
+     * @param _amount: amount to inject in FDEX token
+     * @dev Callable by operator
+     */
+    function injectFunds(uint256 _lotteryId, uint256 _amount) external;
+
+    /**
+     * @notice Start the lottery
+     * @dev Callable by operator
+     * @param _endTime: endTime of the lottery
+     * @param _priceTicketInFDEX: price of a ticket in FDEX
+     * @param _discountDivisor: the divisor to calculate the discount magnitude for bulks
+     * @param _rewardsBreakdown: breakdown of rewards per bracket (must sum to 10,000)
+     * @param _treasuryFee: treasury fee (10,000 = 100%, 100 = 1%)
+     */
+    function startLottery(
+        uint256 _endTime,
+        uint256 _priceTicketInFDEX,
+        uint256 _discountDivisor,
+        uint256[6] calldata _rewardsBreakdown,
+        uint256 _treasuryFee
+    ) external;
+
+    /**
+     * @notice View current lottery id
+     */
+    function viewCurrentLotteryId() external returns (uint256);
+}
