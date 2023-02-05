7215ee4e41fbe046a75391a38d5ab8c2b4930175
garganshu <garganshu96@gmail.com>
2023-02-05 14:28:57 +0530

    refactor: wrap/unwrap ui updated

--- react-app/src/data/make_tokens.js
+++ react-app/src/data/make_tokens.js
@@ -2,6 +2,7 @@ import erc20ABI from "../abi/ERC20ABI";
 import werc20Abi from "../abi/WERC20ABI";
 import makeTokenContract from "../contracts/TokenContract";
 import fileCoinLogo from "../assets/filcoin_logo.svg";
+import FilDexConstants from "../Constants";
 
 const tt1Address = "0x15d471748c0ec3255C1f17158729C989CAe0688E";
 const tt2Address = "0x8D9Cf8B58fcF00Ead8550459778EBd8F188951E4";
@@ -9,7 +10,6 @@ const tt2Address = "0x8D9Cf8B58fcF00Ead8550459778EBd8F188951E4";
 const fUsdAddress = "0x9FB47Fa35ec3BFcE7DbcE3f490D1a389c3891a90";
 const fUsdtAddress = "0x0F6B18b1883aF4e4D6310500736e46EBE35cABA9";
 const fDexAddress = "0x360835e98f054fCBfC97fe6da0AfB1Ff435ce2BB";
-const tFilAddress = "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
 
 export default function makeTokens(web3) {
   return Object.freeze({
@@ -37,7 +37,7 @@ export default function makeTokens(web3) {
       name: "WTFIL",
       logo: fileCoinLogo,
     }),
-    tFil: makeTokenContract(web3, werc20Abi.abi, tFilAddress, {
+    tFil: makeTokenContract(web3, werc20Abi.abi, FilDexConstants.nativeContractAddress, {
       name: "TFIL",
       logo: fileCoinLogo,
     }),
