0568a92d51003d101f22798278733d34d198c5ff
garganshu <garganshu96@gmail.com>
2023-02-05 23:33:25 +0530

    fix: input type to number

--- react-app/src/components/TokenQuantityInput.js
+++ react-app/src/components/TokenQuantityInput.js
@@ -3,7 +3,7 @@ export default function TokenQuantityInput({ onInput }) {
     <div className="flex flex-row border-solid border-grey-font border rounded-lg bg-input-fill focus-visible:ring hover:bg-hover-fill hover:border-hover-stroke flex-grow justify-between">
       <div className="flex flex-col justify-start">
         <input
-          type="text"
+          type="number"
           onChange={(e) => onInput(e.target.value)}
           placeholder="Enter 0.1, 100, 10%..."
           className="bg-inherit border-none text-white rounded-tl-lg placeholder:text-sm placeholder:text-placeholder-dark-text ml-3 mt-1 focus:outline-none"
