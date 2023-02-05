c4510bb73c58127cd2057d318aae9d4ee396175f
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2023-02-05 10:17:48 +0530

    remove unsused import and comments

--- react-app/src/components/NavigationBar.js
+++ react-app/src/components/NavigationBar.js
@@ -4,7 +4,6 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 import appLogo from "../assets/app_logo.svg";
-import pushLogo from "../assets/push_logo.svg";
 import NotificationButton from "./push/NotificationButton";
 
 export default function NavigationBar({ status, connect, chainId, account }) {
@@ -32,22 +31,5 @@ export default function NavigationBar({ status, connect, chainId, account }) {
         />
       </Container>
     </Navbar>
-    // <div className="flex flex-row justify-between my-4 mx-4">
-    //   <div className="flex flex-row text-white items-center">
-    //     <img src={appLogo} alt="FilDex" />
-    //     <div className="ml-12" />
-    //     <div className="mx-4 text-xs">Swap</div>
-    //     <div className="mx-4 text-xs">Supply FIL-FDX</div>
-    //     <div className="mx-4 text-xs">Wrap/UnWrap</div>
-    //     <div className="mx-4 text-xs">Lottery</div>
-    //    
-    //   </div>
-    //   <ConnectWalletButton
-    //     status={status}
-    //     connect={connect}
-    //     account={account}
-    //     chainId={chainId}
-    //   />
-    // </div>
   );
 }
