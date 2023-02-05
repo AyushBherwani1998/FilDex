7d5ba2cd1e9ba1e1f9407302367fe9d35ddde314
AyushBherwani1998 <ayush.bherwani1998@gmail.com>
2023-02-05 21:38:13 +0530

    add draw functionality in close script

--- contracts/lottery/scripts/closeLottery.ts
+++ contracts/lottery/scripts/closeLottery.ts
@@ -5,9 +5,26 @@ const main = async () => {
     const filDexLottery = await ethers.getContractAt("FilDexLottery", filDexLotteryContractAddress);
     let lotteryId = await filDexLottery.currentLotteryId();
 
-    let transaction = await filDexLottery.closeLottery(lotteryId);
-    console.log("Lottery is closed now: ")
-    console.log(transaction.hash);
+    let closeLotteryTransaction = await filDexLottery.closeLottery(lotteryId);
+    let receipt = await closeLotteryTransaction.wait(1);
+    if (receipt.status === 1) {
+        console.log("Lottery is closed now: ");
+        console.log(closeLotteryTransaction.hash);
+
+
+        let drawLotteryTransaction = await filDexLottery.drawFinalNumberAndMakeLotteryClaimable(lotteryId, true);
+        let drawReceiptTransactionReceipt = await closeLotteryTransaction.wait(1);
+        if (receipt.status === 1) {
+            console.log("Lottery is claimable now: ");
+            console.log(drawLotteryTransaction.hash);
+        } else {
+            console.log("Failed to draw lottery: ");
+            console.log(drawLotteryTransaction.hash);
+        }
+    } else {
+        console.log("Failed to close lottery: ");
+        console.log(closeLotteryTransaction.hash);
+    }
 }
 
 main().catch((error) => {
